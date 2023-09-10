5dbbe1ce89230c11cdcdde478560b7a86b385241
talamus1972 <talamus1972@gmail.com>
2023-09-11 01:10:32 +0300

    1

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,11 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
+    // openModalBtn: document.querySelector('[data-menu-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
+    // closeModalBtn: document.querySelector('[data-menu-close]'),
     modal: document.querySelector('[data-modal]'),
+    //  modal: document.querySelector('[data-menu]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
